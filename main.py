7c3638f942b1a9c97df2829945e6c6616ee92c5d
Marximus Maximus <marximus@marximus.com>
2023-02-22 19:38:44 -0800

    warnings cleanup

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from http.server import (
     BaseHTTPRequestHandler          as http_server_BaseHTTPRequestHandler,
 )
 
-from Foundation import *
-from ScriptingBridge import *
+# from Foundation import *
+from ScriptingBridge import SBApplication  # type: ignore[reportGeneralTypesIssues]
 
 from threading import (
     Thread                          as threading_Thread,
@@ -19,12 +19,19 @@ from datetime import (
     datetime                            as datetime_datetime,
 )
 
-appAppleMusic = SBApplication.applicationWithBundleIdentifier_("com.apple.Music")
+from typing import (
+    Any,
+)
+
+appAppleMusic = SBApplication.applicationWithBundleIdentifier_("com.apple.Music")  # type: ignore[reportGeneralTypesIssues]
 
 
 SERVER_PORT = 8080
 
-def commentToStyle(comment):
+BGCOLOR="#6E6856"
+FGCOLOR="29,27,22"
+
+def commentToStyle(comment: str | None):
 
     if comment is None:
         return ""
@@ -68,12 +75,12 @@ class Server(object):
     thread = None
     testCase = None
 
-    #---------------------------------------------------------------------------------------------------------------
-    def __init__(self):
-        pass
+    # #---------------------------------------------------------------------------------------------------------------
+    # def __init__(self):
+    #     pass
 
     #---------------------------------------------------------------------------------------------------------------
-    def runServer(self, handlerCls):
+    def runServer(self, handlerCls: http_server_BaseHTTPRequestHandler):
         self.thread = threading_Thread(
             target=self.serverThread,
             args=
@@ -84,7 +91,7 @@ class Server(object):
         self.thread.start()
 
     #---------------------------------------------------------------------------------------------------------------
-    def serverThread(self, handlerCls):
+    def serverThread(self, handlerCls: Any):
         print("starting server")
 
         server = http_server_HTTPServer(('localhost', SERVER_PORT), handlerCls)
@@ -98,7 +105,8 @@ class Server(object):
     #---------------------------------------------------------------------------------------------------------------
     def stopServer(self):
         self.keepRunning = False
-        self.thread.join()
+        if self.thread is not None:
+            self.thread.join()
 
     pass
 
@@ -110,12 +118,40 @@ class GenericHandler(http_server_BaseHTTPRequestHandler):
     def do_GET(self):
         parsed_path = urllib_parse_urlparse(self.path)
         real_path = parsed_path.path
-        headers = self.headers
+        # headers = self.headers
+
+        currentTrack: Any
+        currentPlaylist: Any
+        playlistTracks: Any
+
+        currentTitle: str | None = ""
+        currentArtist: str | None = ""
+        currentComment: str | None = ""
+        currentLengthPretty: str | None = ""
+        currentPositionAndLengthPretty: str | None = ""
+        currentDanceStyleHeader: str | None = ""
+        currentStyle: str | None = ""
+        currentStart: int = 0
+        currentEnd: int = 0
+        currentPlayerPosition: int = 0
+
+        nextTitle: str | None = ""
+        nextArtist: str | None = ""
+        nextStyle: str | None = ""
+        nextLengthPretty: str | None = ""
+        nextDanceStyleHeader: str | None = ""
+        nextDivider: str | None = ""
+        nextHeader: str | None = ""
+
+        nextNextTitle: str | None = ""
+        nextNextComment: str | None = ""
+        nextNextLengthPretty: str | None = ""
+        nextNextHeader: str | None = ""
 
         if real_path == "/STOP_SERVER":
             return
         else:
-            currentTrack = appAppleMusic.currentTrack()
+            currentTrack = appAppleMusic.currentTrack()  # type: ignore[reportGeneralTypesIssues]
             currentTitle = currentTrack.name()
             # if currentTitle is not None:
             #     currentTitle = currentTitle.encode("utf8", "ignore")
@@ -134,7 +170,7 @@ class GenericHandler(http_server_BaseHTTPRequestHandler):
             currentLengthSeconds = int(currentLengthSeconds % 60)
             currentLengthPretty = "{:d}:{:02d}".format(currentLengthMinutes, currentLengthSeconds)
 
-            currentPlayerPosition = appAppleMusic.playerPosition()
+            currentPlayerPosition = appAppleMusic.playerPosition()  # type: ignore[reportGeneralTypesIssues]
             currentAdjustedPlayerPosition = max(currentPlayerPosition - currentStart, 0)
             currentPositionMinutes = int(currentAdjustedPlayerPosition / 60)
             currentPositionSeconds = int(currentAdjustedPlayerPosition % 60)
@@ -142,7 +178,7 @@ class GenericHandler(http_server_BaseHTTPRequestHandler):
 
             currentPositionAndLengthPretty = "{}/{}".format(currentPositionPretty, currentLengthPretty)
 
-            currentPlaylist = appAppleMusic.currentPlaylist()
+            currentPlaylist = appAppleMusic.currentPlaylist()  # type: ignore[reportGeneralTypesIssues]
             playlistTracks = currentPlaylist.tracks()
 
             currentDanceStyleHeader = "Dance Style Info:"
@@ -184,7 +220,6 @@ class GenericHandler(http_server_BaseHTTPRequestHandler):
             nextNextTitle = ""
             nextNextComment = ""
             nextNextLengthPretty = ""
-            nextNextDivider = ""
             nextNextHeader = ""
 
             nextNextIndex = currentIndex + 2
@@ -213,7 +248,6 @@ class GenericHandler(http_server_BaseHTTPRequestHandler):
                 nextNextLengthSeconds = int(nextNextLengthSeconds % 60)
                 nextNextLengthPretty = " - {:d}:{:02d}".format(nextNextLengthMinutes, nextNextLengthSeconds)
 
-                nextNextDivider = "<hr>"
                 nextNextHeader = "Followed by:<br/>"
 
                 if nextNextTitle in SECRET_TITLES:
@@ -229,7 +263,6 @@ class GenericHandler(http_server_BaseHTTPRequestHandler):
                 nextNextTitle = ""
                 nextNextComment = ""
                 nextNextLengthPretty = ""
-                nextNextDivider = ""
                 nextNextHeader = ""
 
             if (
@@ -257,10 +290,7 @@ class GenericHandler(http_server_BaseHTTPRequestHandler):
                 nextDivider = "<hr>"
                 nextHeader = ""
                 nextNextTitle = ""
-                nextNextArtist = ""
-                nextNextStyle = ""
                 nextNextLengthPretty = ""
-                nextNextDanceStyleHeader = ""
                 nextNextHeader = ""
                 nextNextComment = ""
             elif currentTitle == LAST_DANCE_TITLE:
@@ -303,7 +333,6 @@ class GenericHandler(http_server_BaseHTTPRequestHandler):
 
                 nextNextComment = ""
                 nextNextLengthPretty = ""
-                nextNextDivider = ""
                 nextNextHeader = ""
             else:
                 currentTitle = "<div class=\"title\">" + currentTitle + "</div>"
@@ -312,7 +341,7 @@ class GenericHandler(http_server_BaseHTTPRequestHandler):
             realTime = datetime_datetime.now().strftime("%-I:%M:%S %p")
 
             message = (
-                """
+                f"""
                 <!DOCTYPE html>
                 <html>
                 <head>
@@ -320,115 +349,113 @@ class GenericHandler(http_server_BaseHTTPRequestHandler):
                 <meta http-equiv="refresh" content="1" />
                 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
                 <style>
-                    body {
+                    body {{
                         font-family: Big Caslon, -system, -system-font, Arial;
-                        background-color: #6E6856;
-                        color: rgba(29,27,22, 1);
-                    }
+                        background-color: {BGCOLOR};
+                        color: rgba({FGCOLOR}, 1);
+                    }}
 
-                    body table {
+                    body table {{
                         margin: 0;
                         margin-left: 1%;
                         margin-right: 1%;
-                    }
+                    }}
 
-                    ul {
+                    ul {{
                         -webkit-column-count: 2;
                         margin: 0;
                         margin-left: 0.5em;
-                    }
+                    }}
 
-                    li {
+                    li {{
                         margin-left: 0.5em;
-                    }
+                    }}
 
-                    hr {
+                    hr {{
                         border: 1px;
                         border-style: solid;
                         margin-top: 20px;
                         margin-bottom: 20px;
-                    }
+                    }}
 
-                    .bottom {
+                    .bottom {{
                         position: absolute;
                         top: 600px;
                         width: 1900px;
-                    }
-                    .bottom-right {
+                    }}
+                    .bottom-right {{
                         position: absolute;
                         top: 646px;
                         left: 1000px;
                         width: 800px;
-                    }
+                    }}
 
-                    .currentTitle {
+                    .currentTitle {{
                         font-size: 4vw;
-                    }
-                    .currentTitle div.title {
+                    }}
+                    .currentTitle div.title {{
                         width: 68%;
                         white-space: nowrap;
                         overflow: hidden;
                         text-overflow: ellipsis;
-                    }
-                    .currentTitle div.time {
+                    }}
+                    .currentTitle div.time {{
                         position: absolute;
                         top: 1%;
                         right: 1%;
-                    }
-                    .currentArtist {
+                    }}
+                    .currentArtist {{
                         font-size: 2vw;
-                        color: rgba(29,27,22, 0.8);
-                    }
-                    .currentStyleHeader {
+                        color: rgba({FGCOLOR}, 0.8);
+                    }}
+                    .currentStyleHeader {{
                         font-size: 2vw;
-                        color: rgba(29,27,22, 0.8);
-                    }
-                    .currentStyle {
+                        color: rgba({FGCOLOR}, 0.8);
+                    }}
+                    .currentStyle {{
                         font-size: 4.2vw;
-                    }
+                    }}
 
-                    table.nextTable {
-                        color: rgba(29,27,22, 0.7);
-                    }
+                    table.nextTable {{
+                        color: rgba({FGCOLOR}, 0.7);
+                    }}
 
-                    table.nextNextTable {
+                    table.nextNextTable {{
                         font-size: 1.5vw;
-                        color: rgba(29,27,22, 0.7);
-                    }
+                        color: rgba({FGCOLOR}, 0.7);
+                    }}
 
-                    .headerNext {
+                    .headerNext {{
                         font-size: 2.5vw;
-                    }
-                    .nextTitle {
+                    }}
+                    .nextTitle {{
                         font-size: 2vw;
-                    }
-                    .nextArtist {
+                    }}
+                    .nextArtist {{
                         font-size: 1.5vw;
-                    }
-                    .nextStyleHeader {
+                    }}
+                    .nextStyleHeader {{
                         font-size: 1.5vw;
-                    }
-                    .nextStyle {
+                    }}
+                    .nextStyle {{
                         font-size: 2vw;
-                    }
+                    }}
 
-                    div.realTime {
+                    div.realTime {{
                         position: absolute;
                         bottom: 1%;
                         right: 1%;
                         font-size: 4vw;
-                        color: rgba(29,27,22, 0.4);
-                    }
+                        color: rgba({FGCOLOR}, 0.4);
+                    }}
 
-                    div.bigTitle {
+                    div.bigTitle {{
                         font-size: 7.5vw;
                         text-align: center;
                         width: 100%;
-                    }
+                    }}
                 </style>
                 </head>
-                """ +
-                """
                 <body><div max-width="100%">
                     <table width="98%">
                         <tr><td class="currentTitle">
@@ -487,15 +514,13 @@ class GenericHandler(http_server_BaseHTTPRequestHandler):
                     </div>
                 </div></body>
                 </html>
-                """.format(
-                    **locals()
-                )
+                """
             )
 
             message = message.encode("utf8", errors="ignore")
             self.send_response(200)
             self.end_headers()
-            self.wfile.write(message)
+            _ = self.wfile.write(message)
             return
 
 
